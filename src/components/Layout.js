2a168ccdfbdab4d79116256db0db8a9331eee9c9
maru <include321321321@gmail.com>
2020-01-14 22:46:09 +0900

    add new file

---
+++ src/components/Layout.js
@@ -0,0 +1,17 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./layout.css";
+import Nav from "./Nav";
+
+const Layout = ({ children }) => (
+  <div className="layout">
+    <Nav />
+    <main className="main">{ children }</main>
+  </div>    
+);
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired
+};
+
+export default Layout;
---
+++ src/components/Layout.js~
@@ -0,0 +1,17 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import Nav from "./Nav";
+
+const Layout = ({ children }) => (
+  <>
+    <Nav />
+    <main>{ children }</main>
+  </>
+);
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired
+};
+
+export default Layout;
